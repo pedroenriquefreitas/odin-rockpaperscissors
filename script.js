706d874231c57102e8ddeb061fd316290e6d5d12
RedManBrasil <pefandrade@hotmail.com>
2022-06-27 23:54:03 -0300

    Final project with no view

--- script.js
+++ script.js
@@ -3,29 +3,37 @@ function computerPlay(){
     return options[Math.floor(Math.random() * 3)];
 }
 
-const temp = computerPlay();
-console.log(temp);
-
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
         case "Rock":
-            if (computerSelection == "Rock") {console.log("It's a tie!");}
-            else if (computerSelection == "Scissors") {console.log(`You Win! ${playerSelection} beats ${computerSelection}`);}
-            else if (computerSelection == "Paper") {console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);}
+            if (computerSelection == "Rock") {return("It's a tie!");}
+            else if (computerSelection == "Scissors") {return(`You Win! ${playerSelection} beats ${computerSelection}`);}
+            else if (computerSelection == "Paper") {return(`You Lose! ${computerSelection} beats ${playerSelection}`);}
             break;
         case "Paper":
-            if (computerSelection == "Paper") {console.log("It's a tie!");}
-            else if (computerSelection == "Rock") {console.log(`You Win! ${playerSelection} beats ${computerSelection}`);}
-            else if (computerSelection == "Scissors") {console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);}
+            if (computerSelection == "Paper") {return("It's a tie!");}
+            else if (computerSelection == "Rock") {return(`You Win! ${playerSelection} beats ${computerSelection}`);}
+            else if (computerSelection == "Scissors") {return(`You Lose! ${computerSelection} beats ${playerSelection}`);}
             break;
         case "Scissors":
-            if (computerSelection == "Scissors") {console.log("It's a tie!");}
-            else if (computerSelection == "Paper") {console.log(`You Win! ${playerSelection} beats ${computerSelection}`);}
-            else if (computerSelection == "Scissors") {console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);}
+            if (computerSelection == "Scissors") {return("It's a tie!");}
+            else if (computerSelection == "Paper") {return(`You Win! ${playerSelection} beats ${computerSelection}`);}
+            else if (computerSelection == "Rock") {return(`You Lose! ${computerSelection} beats ${playerSelection}`);}
             break;
         default:
-          console.log(`Please, input a correct value`);
+          return(`Please, input a correct value`);
       }
   }
 
-  playRound("Rock", temp);
+function game(){
+    for (let i = 0; i < 5; i++) {
+        let userSelection = prompt("What's your play (Rock, Paper or Scissors?");
+        while (userSelection != "Rock" && userSelection != "Paper" && userSelection != "Scissors") {
+            userSelection = prompt("Please input a correct value. What's your play (Rock, Paper or Scissors?");
+        }
+        let computerSelection = computerPlay();
+        console.log(playRound(userSelection, computerSelection));
+     }
+}
+
+game();
